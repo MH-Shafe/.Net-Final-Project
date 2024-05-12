542ada9f0e37a9f4008d4b36c7d8914cef0db3bc
MH-Shafe <shafemahfuzulhaque@gmail.com>
2024-05-13 01:21:35 +0600

    12.19am-13/05/2024

--- Uber/Uber/Controllers/LoginController.cs
+++ Uber/Uber/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace Uber.Controllers
@@ -42,7 +43,7 @@ namespace Uber.Controllers
             }
         }
         */
-        /*
+
         [HttpPost]
         [Route("api/login")]
         public HttpResponseMessage Login([FromBody] LoginDTO login)
@@ -57,17 +58,28 @@ namespace Uber.Controllers
                     return Request.CreateResponse(HttpStatusCode.Unauthorized, new { Message = "Invalid username or password" });
                 }
 
-                return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Login successful" });
+                // Set the user's role in a cookie
+                var cookie = new HttpCookie("UserRole");
+                cookie.Value = user.roll;
+                cookie.Expires = DateTime.Now.AddMinutes(5); // Cookie expires in 5 minutes
+                HttpContext.Current.Response.Cookies.Add(cookie);
+
+                // Login successful message
+                var response = Request.CreateResponse(HttpStatusCode.OK, new { Message = "Login successful" });
+
+                // Retrieve role from cookie and append it to the response message
+                var userRole = HttpContext.Current.Request.Cookies["UserRole"]?.Value;
+                if (!string.IsNullOrEmpty(userRole))
+                {
+                    response.Content = new StringContent(response.Content.ReadAsStringAsync().Result + " - User Role: " + userRole);
+                }
+
+                return response;
             }
             catch (Exception ex)
             {
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
             }
         }
-        */
-      
-
-    }     
-    
-
+    }
 }
