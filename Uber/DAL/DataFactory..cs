4489d6cd0f07100d303901f5ef46f3b28e94014b
jacksparrow2160 <121813576+jacksparrow2160@users.noreply.github.com>
2024-05-14 02:02:19 +0600

    repo
    rat 2 ta baje

--- Uber/DAL/DataFactory..cs
+++ Uber/DAL/DataFactory..cs
@@ -1,8 +1,10 @@
 ﻿using DAL.EF;
 using DAL.EF.Entites;
 using DAL.EF.Entites.Admin;
+using DAL.EF.Entites.User;
 using DAL.Interfaces;
 using DAL.Repos;
+using DAL.Repos.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +24,10 @@ namespace DAL
         public static IRepo<SignUp, int> SignUpData()
         {
             return new SignUpRepo(context);
+        }
+        public static IRepo<SingUp_u, int> SignUpData_u()
+        {
+            return new SingRepo_u(context);
         }
     }
 }
--- Uber/DAL/EF/UberContext.cs
+++ Uber/DAL/EF/UberContext.cs
@@ -1,5 +1,6 @@
 ﻿using DAL.EF.Entites;
 using DAL.EF.Entites.Admin;
+using DAL.EF.Entites.User;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -13,6 +14,8 @@ namespace DAL.EF
     {
         public DbSet<Login> Logins { get; set; }
         public DbSet<SignUp> SignUps { get; set; }
+
+        public DbSet<SingUp_u> SignUp_us { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Login>()
